fa229a1b84e341faa90a98cc8a194b204e86cf18
github username <github email address>
2019-04-30 22:25:14 -0400

    messed up

--- mod5project-api/app/models/user.rb
+++ mod5project-api/app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :articles
   has_many :musings
+  has_many :matches
+  has_many :matched_users, through: :matches
+  dependent: :destroy
   has_secure_password
 
   validates :name, :email, :username, :password, presence: true
